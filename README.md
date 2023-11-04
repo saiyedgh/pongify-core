0cea8bb3bf9ca5af362fcaa1ef7371392889701c
Syed Hassan <sai-yed@outlook.com>
2023-11-04 13:52:53 -0400

    feat: working Pongify CLI game

---
+++ README.md
@@ -0,0 +1,100 @@
+---
+Faculty: Faculty of Information
+University: University of Toronto
+Program: BI – Bachelor of Information
+Course: INF452 - Information Design V - Coding
+Instructor: Dr. Maher Elshakankiri
+Student: Syed-Mughira Hassan
+Assignment: Mid-term
+Programming Language: Python
+Date Created: 31-Oct-2023
+Date Modified: 04-Nov-2023
+---
+
+# Pongify - your table tennis game manager
+
+## Documentation
+
+- Pongify is a program written in vanilla Python – majority of the code.
+- It is a is a table tennis schedule manager and points/result calculator.
+- Pongify only features games and tournaments for 2 to 5 players.
+<br>
+
+It's code logic can be divided into three main parts.
+## Part 01 - Scheduler
+- Firstly, it takes user input for the number of players, from *two* to *five*.
+- Based on the number of players, it asks the name of each player, capturing the player names in a list called "**players**".
+
+- Pongify then asks for number of rounds which decides how many matches will the players play with the other(s).
+- The program then creates player combinations using Python [itertools](https://docs.python.org/3/library/itertools.html) library.
+<br>
+
+## Part 02 - Points Table
+
+Pongify then creates a points table (a 2d list) to account for:
+
+- Player names based on the user input from *Part 01*,
+- Player points scored - points for "**PF**",
+- Player points received - points against "**PA**",
+- Player points difference "**PD**",
+- Player Wins/Losses - "**W**" / "**L**",
+- Total player "**Points**" - 3 points for each win.
+<br>
+
+## Part 03 - Match Results and Player Standing
+
+- Pongify then calculates results, match by match.
+- It asks for match results doumenting each player's score.
+- It then calculates the win/loss and the points situation and edits the points table.
+- It prints the results till the last match and player score then outputs the points table where the players can check the results.
+
+## Game Rules
+
+- Table tennis rules [https://www.pongfit.org/official-rules-of-table-tennis](https://www.pongfit.org/official-rules-of-table-tennis).
+- At 11 points if a player has a two point advantage, they win.
+
+## Example Tournament
+
+**To test the program, following is a sample game scenario:**
+
+- Number of players: 3
+- Enter three player names:
+    - player 1: "*Anas*"
+    - player 2: "*Hassan*"
+    - player 3: "*Syed*"
+- Number of rounds: 1
+
+**Enter results:**
+
+- Match 01 -> *Anas* = 11, *Hassan* 9 (*Anas* wins by two points),
+- Match 02 -> *Anas* = 15, *Syed* 13 (*Anas* wins again),
+- Match 03 -> *Hassan* = 11, *Syed* 7 (*Hassan* wins by 4 points),
+- Match 04 -> *Anas* = 11, *Hassan* 5 (*Anas* wins by 6 points),
+- Match 05 -> *Anas* = 9, *Syed* 11 (*Syed* wins by 2 points),
+- Match 06 -> *Hassan* = 15, *Syed* 13 (*Hassan* wins by 2 points),
+- *Anas* should win having 9 match points in total, followed by 6 points for *Hassan*, and 3 points for *Syed*.
+
+## Limitations and upcoming Improvements
+
+- Is limited to single player names not doubles or teams, can be improved to incorporate teams and member names.
+- The current setup does not support editing/updating or deleting results, can add that functionality.
+- The command line interface is not user friendly, can use **Flask** or **Django** to create a web based UI written in Python.
+- Need to improve code validation.
+- Can validate and make sure player names are only strings.
+- Can store calculation logic in a function and use it for the other.
+- The list is not sorted based on results and standings.
+- The final points table rows can be sorted based on points and points difference.
+
+
+## Project Details
+
+Faculty: Faculty of Information
+University: University of Toronto
+Program: BI – Bachelor of Information
+Course: INF452 - Information Design V - Coding
+Instructor: Dr. Maher Elshakankiri
+Student: Syed-Mughira Hassan
+Assignment: Mid-term
+Programming Language: Python
+Date Created: 31-Oct-2023
+Date Modified: 04-Nov-2023
---
+++ pongify.py
@@ -0,0 +1,201 @@
+# Faculty of Information
+# University of Toronto
+# BI – Bachelor of Information
+# Course: INF452 - Information Design V: Coding
+# Instructor: Dr. Maher Elshakankiri
+# Student: Syed-Mughira Hassan
+# Assignment: Mid-term
+# Programming Language: Python
+# Date Created: 31-Oct-2023
+# Date Modified:  04-Nov-2023 
+
+# ----------------------- Pongify - Program ----------------------- #
+
+# ----------------------- Imports #
+# Requiring itertools library for alternative looping to create player combinations
+# https://docs.python.org/3/library/itertools.html
+import itertools
+
+# ----------------------- Part 01 - Scheduler ----------------------- #
+print("") # will use this syntax to print linebreaks for CLI/Game readability
+
+# Input loop to capture player names 
+while True:
+    try: # try to catch integer input
+        numPlayers = int(input("Enter number of players from (2-5): "))
+        # limiting player range from to 2 - 5
+        if numPlayers >= 2 and numPlayers <= 5: # valid input
+            break
+        else: # condition error message
+            print("Must have 2 to 5 players.")
+    except ValueError: # loop error message
+        print("Please enter a valid number.")
+
+players = []
+
+print("")
+
+# Capture player names and edit the players list
+for i in range(numPlayers):
+    userInput = input(f"Enter player name {i + 1}: ")
+    players.append(userInput)
+
+# Asking for number of rounds
+while True:
+    try:
+        print("")
+        # explaining the idea of match rounds
+        print("One round = two matches with each player")
+        print("")
+        # adding 1 to user because one round equals two games with each
+        rounds = int(input("Enter number of rounds from (1-3): ")) + 1
+        # tournament round limit 
+        if rounds >= 1 and rounds <= 3:
+            break
+        else: # condition error message
+            print("Must have 1, 2 or 3 rounds.")
+    except ValueError: # loop error message
+        print("Please enter a valid number.")
+
+# Printing the number of player matches with the other player(s)
+print(f"Matches vs each: {rounds}")
+
+# Creating a function that takes a list of names and number of rounds and creates match combinations
+def playerCombinations(playerList, matchRounds):
+    combinations = []
+    # the loop does not require an iterator variable as that is managed by the itertools library
+    for _ in range(matchRounds):
+        #https://www.geeksforgeeks.org/itertools-combinations-module-python-print-possible-combinations/
+        roundCombinations = list(itertools.combinations(playerList, 2))
+        combinations.extend(roundCombinations)
+    return combinations # value accessible for use
+
+combinations = playerCombinations(players, rounds)
+
+matchList = []
+
+# Taking the combination list and destructuring the input to create our custom nested list called "matchList"
+# using enumerate to adjust the array index and sequence
+# https://www.w3schools.com/python/ref_func_enumerate.asp
+for sequence, (player1, player2) in enumerate(combinations, start=1):
+    matchList.append([player1, player2])
+
+# Printing the player match details
+print("")
+print("**** Table Tennis game tournament schedule ****")
+print(f"Each player plays {rounds} matches with the other player(s):")
+print("")
+
+# Printing the tournament schedule
+# using enumerate with the for loop for easy indexing
+for index, item in enumerate(matchList):
+    player01 = item[0]
+    player02 = item[1]
+    print(f"Match {index + 1}: {player01} vs {player02}")
+
+# ----------------------- Part 02 - Points Table ----------------------- #
+
+# Creating a 2d list with table headers
+tableHeader = ["Name", "W", "L", "PF", "PA", "PD", "Points"]
+pointsTable = [tableHeader] 
+
+# Adding the list rows with player names in the first column and 0s in all the others as a numeric placeholder
+for name in players:
+    row = [name] + [0] * (len(tableHeader) - 1)
+    pointsTable.append(row)
+
+# ----------------------- Part 03 - Match Results and Player Standing ----------------------- #
+
+matchIndex = int(1) # initiating a match counter
+
+# looping through the match list to make changes to the points table according to each match
+for match in matchList:
+    # temp variable to capture player scores
+    # will be used for points table calculations
+    player01Score = int() 
+    player02Score = int()
+    
+    # printing mere linebreak and some text
+    print("")
+    print("Points table: ")
+
+    # Printing results before and after each match
+    for row in pointsTable:
+        print(row)
+    print("")
+
+    # Printing the match details Player01 vs Player02
+    print(f"Match {[matchIndex]}: {match[0]} vs {match[1]}")
+    matchIndex += 1 # add 1 to counter after each match
+
+    # Each match calculation
+    # looping through each match players and record their score
+    for matchPlayer in match:
+        # keep asking for a valid input
+        while True:
+            try:
+                print("")
+                # Asking for player score
+                inputValue = int(input(f"Enter match score of {matchPlayer}: "))
+                # the scores don't usually cross 20 points, hence, 30 is a good limit
+                if inputValue <= 30:
+                    # first player score
+                    if matchPlayer == match[0]:
+                        player01Score = inputValue
+                    # second player score
+                    else:
+                        player02Score = inputValue
+                    break  # Exit the loop if a valid integer score is provided
+                else: # condition error message
+                    print("Points must be less than 30")
+            except ValueError: # while loop error message
+                print("Invalid input. Please enter an integer value.")
+
+    # Points table calculation based on the player01Score and player02Score
+    # looping through the points table and edit results based on match score input
+    for row in pointsTable[1:]: #skipping the table headers
+        # match player names and calculate player 01 results
+        if row[0] == match[0]:
+            row[3] += player01Score # Points for - PF
+            row[4] += player02Score # Points against - PA
+            row[5] = row[3] - row[4] # Points difference - PD
+            # if player01 won
+            if player01Score > player02Score:
+                row[1] += 1  # Increase 'W' or win for player01Score
+            # if player01 lost
+            else:
+                row[2] += 1  # Increase 'L' or loss for the player
+            # Update the Points column based on player win
+            row[6] = row[1] * 3  
+        
+        # match player names and calculate player 02 results
+        elif row[0] == match[1]:
+            row[3] += player02Score # Points for - PF
+            row[4] += player01Score # Points against - PA
+            row[5] = row[3] - row[4] # Points difference - PD
+            # if player02 won
+            if player02Score > player01Score:
+                row[1] += 1  # Increase 'W' or win for player02Score
+            # if player02 lost
+            else:
+                row[2] += 1  # Increase 'L' or loss for the player
+            # Update the Points column based on player win
+            row[6] = row[1] * 3  
+
+# Print the Final result
+print("")
+print("**** The Final Result ****")
+# taking the results from the Points Table
+for row in pointsTable:
+    print(row)
+
+print("")
+
+# Is limited to single player names not doubles or teams, can be improved to incorporate teams and member names
+# The current setup does not support editing/updating or deleting results, can add that functionality
+# The command line interface is not user friendly, can use Flask or Django to create a web based UI written in Python
+# Need to improve code validation
+# Can validate and make sure player names are only strings
+# Can store calculation logic in a function and use it for player02
+# The list is not sorted based on results and standings
+# The final points table rows can be sorted based on points and points difference
